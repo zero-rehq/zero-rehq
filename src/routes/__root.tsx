713db1fda9d699368857fb96760d82f26b568a79
Bruno Murua <b.muruacarreras@gmail.com>
2025-10-21 20:00:59 +0200

    fix: conditionally render TanStackRouterDevtools in development mode

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,7 +5,7 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
-      <TanStackRouterDevtools />
+      {import.meta.env.DEV && <TanStackRouterDevtools />}
     </>
   ),
 })
